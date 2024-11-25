962919dc96b35939a62b8b3dfc384dd9f839c6a4
hommula <77308279+hommula@users.noreply.github.com>
2024-11-25 12:27:22 -0800

    fix rps layout

--- scriptsfolder/main.js
+++ scriptsfolder/main.js
@@ -77,79 +77,17 @@ function goToButtonListener(element)
             element[1].style.display = "block";
             currentPage = element[1];
             typeWriter(element[2]);
-            
         }
     )}
 )}
 
 
-
 // loop through the gotobtn array list and call the add listener function.
 goToBTN.forEach(goToButtonListener);
 
-// goToButtonListener(gofirstButtons, firstPage);
-// goToButtonListener(gosecondButtons, secondPage);
-// goToButtonListener(gothirdButtons, thirdPage);
-// goToButtonListener(gofourthButtons, fourthPage);
-// goToButtonListener(goTTTButtons, tttPage);
-// goToButtonListener(goRpsButtons, rpsPage);
 
-
-
-// gofirstButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         hidepages();
-//         firstPage.style.display = 'block';
-//         count = 0
-//         counter.textContent = count
-//     });
-// });
-
-// gosecondButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         hidepages();
-//         secondPage.style.display = 'block';
-//         count++
-//         counter.textContent = count
-//     });
-// });
-
-// gothirdButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         hidepages();
-//         thirdPage.style.display = 'block';
-//     });
-// });
-
-// gofourthButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         hidepages();
-//         fourthPage.style.display = 'block';
-//         let fourthresult = document.querySelector('#fourthresult');
-//         if (count === 1) {
-//             fourthresult.innerHTML = "you won"
-//         } else if (count === 0) {
-//             fourthresult.innerHTML = ''
-//         }
-//     });
-// });
-
-// goRpsButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         hidepages();
-//         document.querySelector('#rpsPage').style.display = 'block';
-//     });
-// });
-
-// goTTTButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         hidepages();
-//         document.querySelector('#TicTacToePage').style.display = 'block';
-//     })
-// });
-
+// Get all elements with the class 'page' and hide them
 function hidepages() {
-    // Get all elements with the class 'page' and hide them
     const pages = document.querySelectorAll('.page');
     pages.forEach(page => {
         page.style.display = 'none';
